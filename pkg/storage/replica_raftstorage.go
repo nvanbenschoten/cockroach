af2af868c611bf9ed9781de9802966a387c630a0
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2019-09-11 23:47:04 +0000

    Revert "storage: fix ordering of keys put in unreplicatedSST"
    This reverts commit 94ecb469554d23269ed07bcadb8351b900a3f1b7.
    
    Release note: None

--- pkg/storage/replica_raftstorage.go
+++ pkg/storage/replica_raftstorage.go
@@ -837,6 +837,15 @@ func (r *Replica) applySnapshot(
 		return errors.Wrapf(err, "unable to write HardState to unreplicated SST writer")
 	}
 
+	// Update TruncatedState if it is unreplicated.
+	if inSnap.UsesUnreplicatedTruncatedState {
+		if err := r.raftMu.stateLoader.SetRaftTruncatedState(
+			ctx, &unreplicatedSST, s.TruncatedState,
+		); err != nil {
+			return errors.Wrapf(err, "unable to write UnreplicatedTruncatedState to unreplicated SST writer")
+		}
+	}
+
 	// Update Raft entries.
 	var lastTerm uint64
 	var raftLogSize int64
@@ -868,15 +877,6 @@ func (r *Replica) applySnapshot(
 		lastTerm = invalidLastTerm
 	}
 	r.store.raftEntryCache.Drop(r.RangeID)
-
-	// Update TruncatedState if it is unreplicated.
-	if inSnap.UsesUnreplicatedTruncatedState {
-		if err := r.raftMu.stateLoader.SetRaftTruncatedState(
-			ctx, &unreplicatedSST, s.TruncatedState,
-		); err != nil {
-			return errors.Wrapf(err, "unable to write UnreplicatedTruncatedState to unreplicated SST writer")
-		}
-	}
 
 	if err := inSnap.SSSS.WriteSST(ctx, &unreplicatedSST); err != nil {
 		return err
