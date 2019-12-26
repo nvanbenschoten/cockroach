edf88b43b600e2413b5bca964ce8eef1351ec155
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2019-12-26 17:56:02 -0500

    [DNM] storage/result: add UpdatedIntents and ResolvedIntents to LocalResult
    This allows the handling of added, updated, and resolved intents to
    become more reactive. It mirrors our handling of UpdatedTxns and their
    interaction with the TxnWaitQueue.

--- pkg/storage/batcheval/cmd_resolve_intent.go
+++ pkg/storage/batcheval/cmd_resolve_intent.go
@@ -88,6 +88,7 @@ func ResolveIntent(
 	}
 
 	var res result.Result
+	res.Local.ResolvedIntents = []roachpb.Intent{intent}
 	res.Local.Metrics = resolveToMetricType(args.Status, args.Poison)
 
 	if WriteAbortSpanOnResolve(args.Status, args.Poison, ok) {
