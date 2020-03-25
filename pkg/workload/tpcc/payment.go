7c7d88fa56dfab8262dd16e99560c9471aeb2f74
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2020-03-24 20:23:16 -0400

    wip

--- pkg/workload/tpcc/payment.go
+++ pkg/workload/tpcc/payment.go
@@ -114,7 +114,8 @@ func createPayment(ctx context.Context, config *tpcc, mcp *workload.MultiConnPoo
 		SELECT c_id
 		FROM customer
 		WHERE c_w_id = $1 AND c_d_id = $2 AND c_last = $3
-		ORDER BY c_first ASC`,
+		ORDER BY c_first ASC
+		FOR UPDATE`,
 	)
 
 	// Update customer with payment.
